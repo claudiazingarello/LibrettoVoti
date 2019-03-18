c1dd338b7fbd7e5597f3679f715cc92f6efa2ec3
claud <claud@LAPTOP-3MJQ6N3R>
2019-03-18 22:40:16 +0100

    implementato il punto 1

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,30 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
 
+	private List<Voto> voti;
+
+	public Libretto() {
+		this.voti= new ArrayList<Voto>();
+
+	}
+
+	public void add(int voto, String corso, LocalDate data) {
+	
+	}
+	
+	/**
+	 * Aggiunge un nuovo voto al libretto
+	 * 
+	 * @param v il {@link Voto} da aggiungere
+	 */
+	public void add(Voto v) { //piu utilizzabile
+		voti.add(v);
+		//deleghiamo alla lista voti l'aggiunta del voto
+		//lista ordinata per ordine di inserimento
+	}
 }

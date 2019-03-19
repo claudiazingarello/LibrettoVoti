faf59df9c8f8f8af0f9d50ced95789360d04f285
claud <claud@LAPTOP-3MJQ6N3R>
2019-03-19 19:52:03 +0100

    Completato punto 2

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -14,9 +14,9 @@ public class Libretto {
 	}
 
 	public void add(int voto, String corso, LocalDate data) {
-	
+
 	}
-	
+
 	/**
 	 * Aggiunge un nuovo voto al libretto
 	 * 
@@ -26,5 +26,29 @@ public class Libretto {
 		voti.add(v);
 		//deleghiamo alla lista voti l'aggiunta del voto
 		//lista ordinata per ordine di inserimento
+	}
+
+	//public void StampaVoti (int voto) {} //stampa direttamente i voti
+
+	//public StringStampaVoti2 (int voto) {} //costruisce una stringa con gli esami da stampare ma non stampa ancora
+	
+	/**
+	 * Seleziona il sottoinsieme di voti che hanno il punteggio specificato
+	 * 
+	 * @param punti punteggio da ricerca
+	 * @return lista di {@link Voto} aventi quel punteggio (eventualmente vuota)
+	 */
+	public List<Voto> cercaVoti (int punti){ //seleziona una lista che corrisponde ai criteri di ricerca
+	List<Voto> result = new ArrayList<Voto>();
+
+	for(Voto v: this.voti) {
+		//scandisci lista voti
+		//se è uguale a quello passato da parametro
+		//aggiungi a lista risultati fatta di oggetti voto 
+		if(v.getPunti()==punti) {
+			result.add(v);
+		}
+	}
+	return result;
 	}
 }
--- src/it/polito/tdp/libretto/TestLibretto.java
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.libretto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -11,7 +12,7 @@ public class TestLibretto {
 		
 		Libretto libr = new Libretto();
 		
-		//crea 10 nuovi voto e aggiungi a classe Libretto
+		//crea 10 nuovi voti e aggiungi a classe Libretto
 		libr.add(new Voto(30, "Analisi I", LocalDate.of(2017, 1, 15)));
 		libr.add(new Voto(21, "Analisi II", LocalDate.of(2018, 1, 25)));
 		libr.add(new Voto(25, "Fisica I", LocalDate.of(2017, 6, 10)));
@@ -23,6 +24,8 @@ public class TestLibretto {
 		libr.add(new Voto(25, "Logistica", LocalDate.of(2019, 2, 1)));
 		libr.add(new Voto(27, "Programmazione a Oggetti", LocalDate.of(2019, 1, 25)));
 		
+		List<Voto> venticinque = libr.cercaVoti(25);
+		System.out.println(venticinque);
 		
 	}
 
